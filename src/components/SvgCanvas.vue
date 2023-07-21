0b41f8be684614ef47c11fc77ad51aae23690aa0
CatsJuice <wp1298554944@hotmail.com>
2023-07-22 00:14:25 +0800

    fix: update preview when replay config changed

--- src/components/SvgCanvas.vue
+++ src/components/SvgCanvas.vue
@@ -70,6 +70,8 @@ const brushworkLines = computed(() => lines.value.map((line) => {
   return { d }
 }))
 
+watch(() => props.replayOptions, submitSvg, { deep: true })
+
 const onMousedown = (e: MouseEvent) => onDrawStart({ x: e.clientX, y: e.clientY })
 const onMousemove = (e: MouseEvent) => onDraw({ x: e.clientX, y: e.clientY })
 const onMouseup = (_: MouseEvent) => onDrawEnd()
